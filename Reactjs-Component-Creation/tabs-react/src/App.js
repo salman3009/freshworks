50f9bd508575fc9e2dec0942cfd6dc81d170dbda
salman3009 <salman444g@gmail.com>
2024-09-08 20:35:37 +0530

    code

--- Reactjs-Component-Creation/tabs-react/src/App.js
+++ Reactjs-Component-Creation/tabs-react/src/App.js
@@ -1,82 +1,53 @@
 import './App.css';
 import { useState } from 'react';
-
 function App() {
-
-  const [getList, setList] = useState([{
-    stage: "Request submitted",
-    city: "New york",
-    code: "completed"
-  },
-  {
-    stage: "Package received",
-    city: "New york",
-    code: "progress"
-  },
-  {
-    stage: "Transit",
-    city: "New york",
-    code: "backlog"
-  }
-
-  ]);
-
   return (
     <div className="container">
-      {getList.map((obj, index) => {
-        return (<div className="box" keys={index}>
 
-          {obj.code == "completed" && <div className="wrapper">
+      <div className="box">
+        <h1 className="header">Square</h1>
+        <div className="square"></div>
+      </div>
 
-            <div>
-              <div className='full-circle'>
+      <div className="box">
+        <h1 className="header">Rectangle</h1>
+        <div className="rectangle"></div>
+      </div>
 
-              </div>
-              <div className='vertical-line-blue'>
+      <div className="box">
+        <h1 className="header">Oval</h1>
+        <div className="oval"></div>
+      </div>
 
-              </div>
-            </div>
-            <div>
-              <h1>{obj.stage}</h1>
-              <h3>{obj.city}</h3>
-            </div>
-          </div>}
-
-          {obj.code == "progress" && <div className="wrapper">
-
-            <div>
-              <div className='outer-circle-blue'>
-
-              </div>
-              <div className='vertical-line-grey'>
-
-              </div>
-            </div>
-            <div>
-              <h1>{obj.stage}</h1>
-              <h3>{obj.city}</h3>
-            </div>
-          </div>}
+      <div className="box">
+        <h1 className="header">circle</h1>
+        <div className="circle"></div>
+      </div>
 
 
-          {obj.code == "backlog" && <div className="wrapper">
+      <div className="box">
+        <h1 className="header">trapezoid</h1>
+        <div className="trapezoid"></div>
+      </div>
 
-            <div>
-              <div className='outer-circle-grey'>
 
-              </div>
-              <div className='vertical-line-grey'>
+      <div className="box">
+      <h1 className="header">Triangle up</h1>
+      <div className="triangle-up"></div>
+      </div>
+      <div className="box">
+      <h1 className="header">Triangle down</h1>
+      <h1 className="triangle-down"></h1>
+      </div>
+      <div className="box">
+      <h1 className="header">Triangle right</h1>
+      <h1 className="triangle-right"></h1>
 
-              </div>
-            </div>
-            <div>
-              <h1>{obj.stage}</h1>
-              <h3>{obj.city}</h3>
-            </div>
-          </div>}
-        </div>)
-      })}
-
+      </div>
+      <div className="box">
+      <h1 className="header">Triangle left</h1>
+      <h1 className="triangle-left"></h1>
+      </div>
 
     </div>
   )
